e80d467b7e2580dd85fefc68340104a1732d6d66
Jagruti Akkisetty <110735182+7j7a7@users.noreply.github.com>
2023-09-21 19:02:30 +0100

    Add files via upload

---
+++ action.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+
+
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
