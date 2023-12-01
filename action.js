8f52db4861e34034d46fa5f06f3686a47711ba97
Jagruti Akkisetty <110735182+7j7a7@users.noreply.github.com>
2023-12-01 01:29:54 +0000

    Update action.js

--- action.js
+++ action.js
@@ -8,13 +8,34 @@ function closeNav() {
 }
 
 
-
-
-
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
+  }
+
+
+  function signUp(){
+    let email = document.getElementById("email").value;
+    // alert(email);
+    let username = document.getElementById("username").value;
+    // alert(username);
+    let password = document.getElementById("password").value;
+    alert(password);
+    //claer content of div
+    var del = document.getElementById("del");
+    del.innerHTML = "";
+    //add new div
+    var signedUp = document.createElement("div");
+    signedUp.className  = "info";
+    signedUp.id = "change";
+    signedUp.innerHTML = "Sign up successful";
+
+    // document.body.appendChild(signedUp);
+    del.appendChild(signedUp); 
+    //new "Sign up successful" div is added as a child to the "del" div. 
+    //This assumes that the "del" div is the one you want to replace the content of 
+
   }
